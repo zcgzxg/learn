a4152a443b411227c862e4b8e51eb1529c006158
Yeloend <41514616+Yeloend@users.noreply.github.com>
2022-08-22 23:16:41 +0800

    08-22

--- src/views/AntComment.vue
+++ src/views/AntComment.vue
@@ -1,62 +1,100 @@
 <template>
-  <a-card>
-    <template #cover>
-      <a-comment>
-        <template #actions>
-          <span key="comment-basic-like">
-            <a-tooltip title="Like">
-              <template v-if="action === 'liked'">
-                <LikeFilled @click="like" />
-              </template>
-              <template v-else>
-                <LikeOutlined @click="like" />
-              </template>
+  <a-space style="padding-top: 20px" direction="vertical" :size="24">
+    <a-card hoverable style="width: 95%">
+      <template #cover>
+        <a-comment>
+          <template #actions>
+            <span key="comment-basic-like">
+              <a-tooltip title="Like">
+                <template v-if="action === 'liked'">
+                  <LikeFilled @click="like" />
+                </template>
+                <template v-else>
+                  <LikeOutlined @click="like" />
+                </template>
+              </a-tooltip>
+              <span style="padding-left: 8px; cursor: auto">{{ likes }}</span>
+            </span>
+            <span key="comment-basic-dislike">
+              <a-tooltip title="Dislike">
+                <template v-if="action === 'disliked'">
+                  <DislikeFilled @click="dislike" />
+                </template>
+                <template v-else>
+                  <DislikeOutlined @click="dislike" />
+                </template>
+              </a-tooltip>
+              <span style="padding-left: 8px; cursor: auto">{{
+                dislikes
+              }}</span>
+            </span>
+            <span key="comment-basic-reply-to">Reply to</span>
+          </template>
+          <template #author>
+            <a>Ginlon</a>
+          </template>
+          <template #avatar>
+            <a-avatar :src="require('../assets/portrait.png')" alt="Han Solo" />
+          </template>
+          <template #content>
+            <p>
+              We supply a series of design principles, practical patterns and
+              high quality design resources (Sketch and Axure), to help people
+              create their product prototypes beautifully and efficiently.
+            </p>
+          </template>
+          <template #datetime>
+            <a-tooltip :title="moment().format('YYYY-MM-DD HH:mm:ss')">
+              <span>{{ moment().fromNow() }}</span>
             </a-tooltip>
-            <span style="padding-left: 8px; cursor: auto">{{ likes }}</span>
-          </span>
-          <span key="comment-basic-dislike">
-            <a-tooltip title="Dislike">
-              <template v-if="action === 'disliked'">
-                <DislikeFilled @click="dislike" />
-              </template>
-              <template v-else>
-                <DislikeOutlined @click="dislike" />
-              </template>
-            </a-tooltip>
-            <span style="padding-left: 8px; cursor: auto">{{ dislikes }}</span>
-          </span>
-          <span key="comment-basic-reply-to">Reply to</span>
-        </template>
-        <template #author>
-          <a>Ginlon</a>
-        </template>
-        <template #avatar>
-          <a-avatar :src="require('../assets/portrait.png')" alt="Han Solo" />
-        </template>
-        <template #content>
-          <p>
-            We supply a series of design principles, practical patterns and high
-            quality design resources (Sketch and Axure), to help people create
-            their product prototypes beautifully and efficiently.
-          </p>
-        </template>
-        <template #datetime>
-          <a-tooltip :title="moment().format('YYYY-MM-DD HH:mm:ss')">
-            <span>{{ moment().fromNow() }}</span>
-          </a-tooltip>
-        </template>
-      </a-comment>
-    </template>
-    <a-card-meta title="Comment 评论" description="普通的评论">
-      <template #avatar>
-        <a-avatar
-          :src="require('../assets/portrait.png')"
-          alt="Han Solo"
-          size="large"
-        />
+          </template>
+        </a-comment>
       </template>
-    </a-card-meta>
-  </a-card>
+      <a-card-meta title="Comment 评论" description="普通的评论">
+        <template #avatar>
+          <a-avatar
+            :src="require('../assets/portrait.png')"
+            alt="Han Solo"
+            size="large"
+          />
+        </template>
+      </a-card-meta>
+    </a-card>
+    <a-card hoverable style="width: 95%" title="列表评论">
+      <template #cover>
+        <a-list
+          class="comment-list"
+          :header="`${data.length} replies`"
+          item-layout="horizontal"
+          :data-source="data"
+        >
+          <template #renderItem="{ item }">
+            <a-list-item>
+              <a-comment :author="item.author" :avatar="item.avatar">
+                <template #actions>
+                  <span v-for="(action, index) in item.actions" :key="index">{{
+                    action
+                  }}</span>
+                </template>
+                <template #content>
+                  <p>
+                    {{ item.content }}
+                  </p>
+                </template>
+                <template #datetime>
+                  <a-tooltip
+                    :title="item.datetime.format('YYYY-MM-DD HH:mm:ss')"
+                  >
+                    <span>{{ item.datetime.fromNow() }}</span>
+                  </a-tooltip>
+                </template>
+              </a-comment>
+            </a-list-item>
+          </template>
+        </a-list>
+      </template>
+    </a-card>
+  </a-space>
 </template>
 <script>
 import moment from 'moment'
@@ -99,8 +137,28 @@ export default defineComponent({
       like,
       dislike,
       moment,
+      data: [
+        {
+          actions: ['Reply to'],
+          author: 'Han Solo',
+          avatar:
+            'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+          content:
+            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+          datetime: moment().subtract(1, 'days'),
+        },
+        {
+          actions: ['Reply to'],
+          author: 'Han Solo',
+          avatar:
+            'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+          content:
+            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+          datetime: moment().subtract(2, 'days'),
+        },
+      ],
     }
   },
 })
 </script>
-
+<style scoped></style>
